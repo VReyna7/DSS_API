d3bcf7bdacbe2840187b05a1a916345cdb8bb32b
Juan <89045210+VReyna7@users.noreply.github.com>
2023-04-21 23:31:09 -0600

    Update bienve.js

--- Interfaz_API_DSS/js/bienve.js
+++ Interfaz_API_DSS/js/bienve.js
@@ -3,5 +3,5 @@ const boton = document.querySelector('#boton');
 
 boton.addEventListener('click', function() {
     alert('Comenzando...');
-    window.location.href = "conte.php";
+    window.location.href = "Interfaz_API_DSS/conte.php";
 });
